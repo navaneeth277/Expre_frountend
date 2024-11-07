62bd4b7352dc380b5db6d46d5c115e92f54ac624
Navaneeth2707 <162255937+Navaneeth2707@users.noreply.github.com>
2024-11-07 15:24:11 +0530

    Update questions.js

--- src/compounts/questions.js
+++ src/compounts/questions.js
@@ -15,9 +15,9 @@ export default function Question() {
     e.preventDefault();
 
     try {
-      await axios.post('http://localhost:5000/', { data: inputValue });
+      await axios.post('https://exper-backend.onrender.com/', { data: inputValue });
 
-      const response = await axios.get('http://localhost:5000/');
+      const response = await axios.get('https://exper-backend.onrender.com/');
       setReceivedData(response.data.data);
 
       setSubmitted(true);
