c1352bc63025e7b38afd2dba3f2de4d93b50dcb9
Navaneeth2707 <162255937+Navaneeth2707@users.noreply.github.com>
2024-11-07 15:19:49 +0530

    Update keyworbs.js

--- src/compounts/keyworbs.js
+++ src/compounts/keyworbs.js
@@ -15,9 +15,9 @@ export default function Keyworbs() {
     e.preventDefault();
 
     try {
-      await axios.post('http://localhost:5000/', { data: `make the keywords from${inputValue}` });
+      await axios.post('https://exper-backend.onrender.com/', { data: `make the keywords from${inputValue}` });
 
-      const response = await axios.get('http://localhost:5000/');
+      const response = await axios.get('https://exper-backend.onrender.com/');
       setReceivedData(response.data.data);
 
       setSubmitted(true);
