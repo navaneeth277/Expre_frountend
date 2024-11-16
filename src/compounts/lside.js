b329b88c72940f1675a81e827d00576d343b7d52
navaneeth277 <133931199+navaneeth277@users.noreply.github.com>
2024-11-16 12:20:55 +0530

    Update lside.js

--- src/compounts/lside.js
+++ src/compounts/lside.js
@@ -8,34 +8,32 @@ export default function Lside() {
 
   const handleClick = (index) => {
     setActiveButton(index);
-    if (index === 0) { // Assuming you want to navigate to Rside when the first button is clicked
+    if (index === 0) {
       navigate('/questions');
-    }
-    
-    else if(index===1)
-        navigate('Subject_wise')
-    else if(index===2)
-        navigate('/discribe')
-    else if(index===3)
-        navigate('/Summarize')
-    else if(index===4)
-        navigate('/keyworbs')
-    else if(index===5)
-        navigate('/importantpoints')
-    else if(index===6)
-        navigate('/Essay')
-    else if(index===7)
+    } else if (index === 1) {
+      navigate('/subjectwise');
+    } else if (index === 2) {
+      navigate('/describe');
+    } else if (index === 3) {
+      navigate('/summarize');
+    } else if (index === 4) {
+      navigate('/keywords');
+    } else if (index === 5) {
+      navigate('/importantpoints');
+    } else if (index === 6) {
+      navigate('/essay');
+    } else if (index === 7) {
       navigate('/code');
-    else if(index===8)
-        navigate('/pdf')
-    else
-    navigate('/rside');
-    // Add more conditions if other buttons should navigate to different pages
+    } else if (index === 8) {
+      navigate('/pdf');
+    } else {
+      navigate('/rside');
+    }
   };
 
   return (
     <div className='Lside-buttons'>
-      {['Question', 'Subject_wise', 'Describe', 'Summarize', 'Keywords', 'Important points', 'Essay','Code', 'PDF'].map((label, index) => (
+      {['Question', 'Subject Wise', 'Describe', 'Summarize', 'Keywords', 'Important Points', 'Essay', 'Code', 'PDF'].map((label, index) => (
         <button
           key={index}
           className={`L-Button ${activeButton === index ? 'active' : ''}`}
@@ -43,7 +41,6 @@ export default function Lside() {
         >
           <p>{label}</p>
         </button>
-        
       ))}
     </div>
   );
